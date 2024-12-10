cb4b61979979b3ada28af7472bfce69a08c1b26f
linh <linh.pham@yale.edu>
2024-12-09 22:56:22 -0500

    copying code

--- 01_image_cleaning.py
+++ 01_image_cleaning.py
@@ -8,8 +8,31 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 from PIL import Image
 from skimage import img_as_float 
+from IPython.display import display
 
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = ROOT_PATH + '/data/'
 
-DATA_DIR = "../data"
 img_data = {} 
+
+for folder in os.listdir(DATA_DIR): 
+    img_path = DATA_DIR + folder + '/'
+    img_data[folder] = [img_path + img for img in os.listdir(img_path)]
+    
+
+#Sample some images 
+for i, folder in enumerate(img_data.keys()): 
+    print(folder)
+    sample_size = 10 
+    ims = [Image.open(x) for x in random.sample(img_data[folder], sample_size)]
+    im_size = 128 
+    new_im = Image.new('RGB', (im_size*sample_size, im_size))
+    x_offset = 0 
+    for i in ims: 
+        i.thumbnail((im_size, im_size))
+        new_im.paste(i, (x_offset, 0))
+        x_offset += i.size[0]
+   
+    display(new_im) 
+    
 
--- final_report.md
+++ final_report.md
@@ -4,3 +4,13 @@ Project Planning: https://docs.google.com/document/d/1lJUFYhmdQB7gYxt-_zQwv_a1UU
 
 # EENG 439 Final Project 
 by Eric Yoon and Linh Pham 
+
+**Goal**: Classify Yale images and non-Yale images 
+
+Method: 
+- Finetune ResNet and VGG models for classification 
+- Training on Yale campus related images 
+
+1. Need to see how non-finetuned models work on training Yale/Non Yale 
+2. Train model 
+3. Compare final results 
