d06a3ee7b580bdae3cdd1529516cd43026be4fe3
linh <linh.pham@yale.edu>
2024-12-09 22:18:20 -0500

    image cleaning code

---
+++ 01_image_cleaning.py
@@ -0,0 +1,15 @@
+
+#libraries
+import os 
+import random 
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+from PIL import Image
+from skimage import img_as_float 
+
+
+DATA_DIR = "../data"
+img_data = {} 
+
