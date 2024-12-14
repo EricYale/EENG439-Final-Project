1f72cf6291b76f91a10596bd91eda4a2f97c1e02
Eric Yoon <emjyoon@gmail.com>
2024-12-14 16:16:44 -0500

    Interactive Classifier

--- interactive_classifier.py
+++ interactive_classifier.py
@@ -14,10 +14,6 @@ base_model = tf.keras.applications.MobileNetV2(
     weights='imagenet'
 )
 
-# NOTE:
-# We need the correct version of tensorflow to load this model
-# Maybe add a requirements.txt
-base_model = base_model.load_weights("results/imagenet/trained_model/epoch_20.weights.h5")
 
 preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
 global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
@@ -29,23 +25,35 @@ x = base_model(x, training=False)
 x = global_average_layer(x)
 outputs = prediction_layer(x)
 model = tf.keras.Model(inputs, outputs)
+model.load_weights("results/imagenet/trained_model/epoch_20.weights.h5")
 
 def classify_image(filename):
     img = image.load_img(filename, target_size=IMG_SIZE)
     img_array = image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
-    img_array /= 255.0
 
     prediction = model.predict(img_array)
-    confidence = prediction[0][0] > 0.5
+    confidence = prediction[0][0]
     return confidence
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Usage: python classify.py <image_filename>")
         sys.exit(1)
 
-    filename = sys.argv[1]
-    confidence = classify_image(filename)
-    predicted_class = "Yale" if confidence > 0.5 else "Not Yale"
-    print(f"Predicted class: {predicted_class} (confidence: {confidence})")
+    filenames = sys.argv[1:]
+
+    predicted_yale = 0
+    predicted_non_yale = 0
+    for filename in filenames:
+        confidence = classify_image(filename)
+        predicted_class = "Yale" if confidence > 0.90 else "Not Yale"
+        print(f"{filename} :: Predicted class: {predicted_class} (confidence: {confidence})")
+
+        if predicted_class == "Yale":
+            predicted_yale += 1
+        else:
+            predicted_non_yale += 1
+    
+    print(f"{predicted_yale} Yale, {predicted_non_yale} Non-Yale")
+
